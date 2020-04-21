e446961f203c8c9217e6ddb5210274b1298b3e4f
amindevelops <amin.imagine@gmail.com>
2020-04-21 11:19:31 +0000

    Added more notes to JS

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ function commandBuy(flags_values) {
   //Break down the array further
 }
 
+//Objectives:
+//1. Learn the coding styles/rules/nomenclature etc (e.g. -flag, -g for output beautifiers [mysql db]) for command line interfaces (CLI)
+//2. Instead of memorising a bash cheatsheet, learn it while you create your own terminal app
+//3. Explore programming principles
+//4. Opportunity to build YOUR OWN TOOL
+//5. "I built my own console application to speed up my own workflows, in and out of work"
+//6. Successes and Challenges
+// - Employer workflow improvements / barriers
+// --- Console App OR Employer tool e.g. Bitrix, Zapier
+
 //HTML
 //on keypress enter to trigger function
 //Or implement that javascript
@@ -27,3 +37,18 @@ function commandBuy(flags_values) {
 //String operations e.g. explode, split
 //Arrays and access
 //FileReader API
+
+//LocalStorage
+//1. Just in case the code breaks whilst running, you might encounter one of two scenarios:
+// a) partially written data to the file
+// b) no data saved to file
+//LOST OR CORRUPT data
+//Options:
+//a) Create a queue locally on the user's device, which is then processed.
+//b) 
+
+
+//Stretch
+//1. Add a status icon on the app to indicate connectivity strength or presence
+//2. Sync animation or something to indicate to the user when a cloud-sync is taking place between local storage and the server-side
+//3. In that respect, for good UX, you should have an indicator as well to clarify if you are up-to-date with the cloud or there is a local backlog.
